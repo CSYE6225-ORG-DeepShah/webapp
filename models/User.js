384576c4cbda149fc9b89a855cbc979f6a211304
Deep Shah <deepshah@Deeps-MacBook-Air.local>
2023-10-04 19:24:57 -0400

    Creating Models and Database for User and Accounts

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/database');
+const Assignment = require('./Assignment');
+
+// Define the User model with its attributes and data types
+const User = sequelize.define('user', {
+    userID: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    freezeTableName: true,
+});
+
+module.exports = User;
