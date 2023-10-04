583ce8d83b9d5f0294560c82ccdf9dd33e7abaa8
Deep Shah <deepshah@Deeps-MacBook-Air.local>
2023-10-04 19:27:31 -0400

    Creating Authentication for User

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+
+
+const basicAuth = async (req, res, next) => {
+    
+    // check for basic auth header
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        return res.status(401).json({ message: 'Authorization Required' });
+    }
+
+    // verify auth credentials
+    const base64Credentials =  req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [email, password ] = credentials.split(':');
+    console.log('User Email:', email);
+    console.log('User Password', password);
+
+    const user = await User.findOne({where: { email: email }});
+
+    if(!user){
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
+    
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if(!validPassword){
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
+
+    req.user = user;
+
+    next();
+}
+
+module.exports = basicAuth;
